d41cd5a3b170ffaae5f3110c56153472fc5786b6
azijulhakimbd <azijulhakim.sherpur@gmail.com>
2025-09-20 23:21:14 +0600

    [Add]:Todo Handler

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const port = 3000;
+const todoHandler= require('./RouteHandler/todoHandler')
 // ------------- Middleware ----------------------------
 const app = express();
 app.use(express.json())
 
 // ---------------- Database Connection -----------------
-mongoose.connect('mongodb://localhost/todo')
+mongoose.connect('mongodb://localhost/myDB')
   .then(()=> console.log('Connection Successful')
   )
   .catch( err => console.log(err)
@@ -15,7 +16,7 @@ mongoose.connect('mongodb://localhost/todo')
 
 
 // ------------- Application Routes --------------------
-
+app.use('/todo', todoHandler);
 
 // ------------- default error handler ------------------ 
 function errorHandler( err, req, res, next){
